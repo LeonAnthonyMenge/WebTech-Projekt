c00cd4c8590547e0e314eee97a60c202e08170cc
Mike Peters <Maik.Peters@Student.HTW-Berlin.de>
2023-06-14 18:17:49 +0200

    Changes by leona

--- src/test/java/com/example/WebTech/Projekt/WebTechProjektApplicationTests.java
+++ src/test/java/com/example/WebTech/Projekt/WebTechProjektApplicationTests.java
@@ -3,10 +3,8 @@ package com.example.WebTech.Projekt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class WebTechProjektApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
